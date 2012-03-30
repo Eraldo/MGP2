c6d538dbef94200eaa2ce3983dab55855e197b80
Eraldo Helal <eraldo@eraldo.org>
2012-03-30 08:05:28 +0200

    added spaces for readability between functions
    (and similar elements of code)

--- server/server.py
+++ server/server.py
@@ -6,12 +6,15 @@ Created on Mar 23, 2012
 
 import Pyro4, pickle
 
+
 class items():    
     '''
     Representing a collection of items.
     '''
     items = ['task 1', 'task 2', 'task 3']
     filename = "items"
+
+
     def show(self):
         '''
         Output list of items on server.
@@ -19,11 +22,15 @@ class items():
         for i, v in enumerate(self.items):
             print(i, ": ", v)
         #print(*self.items, sep='\n')
+
+
     def get(self):
         '''
         Returns the list of items from server.
         '''
         return self.items
+
+    
     def add(self, item):
         '''
         Add a new item to the list of items on server.
@@ -33,6 +40,8 @@ class items():
         msg = "added: {0}".format(item)
         print(msg)
         return msg
+    
+    
     def delete(self, index):
         '''
         Delete item with given index from the list of items on server.
@@ -45,6 +54,8 @@ class items():
         else:
             msg = "Invalid item number."
         return msg
+
+    
     def save(self):
         '''
         Save the items collection into a pickled file on server.
@@ -54,6 +65,8 @@ class items():
         msg = "saved items to: '{0}'".format(self.filename)
         print(msg)
         return msg
+    
+    
     def load(self):
         '''
         Load the items collection from a pickled file on server.
@@ -67,12 +80,14 @@ class items():
         print(msg)
         return msg
 
+
 def start_name_server():
     '''
     Start name server in shell.
     '''
     from subprocess import call
     call(["/usr/local/bin/python3", "-m", "Pyro4.naming"])
+
 
 def start_server(obj, name):
     print("Starting server..")
@@ -82,6 +97,7 @@ def start_server(obj, name):
     ns.register(name, uri)          # register the object with a name in the name server
     print("..server started.")
     daemon.requestLoop()            # start the event loop of the server to wait for calls
+
 
 def main():
     '''
@@ -93,6 +109,7 @@ def main():
     items_obj.show()
 #    start_name_server()
     start_server(items_obj, "items")
+
 
 if __name__ == "__main__":
     main()
